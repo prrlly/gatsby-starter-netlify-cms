b92ba51a5a4db009b616a00791f5534c28787e24
DDDD <96461773+aurevae@users.noreply.github.com>
2022-06-27 13:38:15 +0800

    update subscribe

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,13 @@ const IndexPage = ({ data }) => {
   };
   const subscribe = () => {
     console.log(value)
+    fetch(`/.netlify/functions/subscribe?email=${value}`)
+      .then(response => response.json())
+      .then((res) => {
+        if (res.status === "OK") {
+          alert("subscribe success")
+        }
+      })
   };
   return (
     <Layout>
@@ -22,19 +29,21 @@ const IndexPage = ({ data }) => {
             <h2 className="title">PRINCIPLE <span className="title-right">VENTURES</span></h2>
             <div className="desc">EXPLORING PHRONESIS IN <span className="desc-right">DIGITAL INNOVATION</span></div>
           </div>
-          <form className="form" action="https://icloud.us8.list-manage.com/subscribe/post?u=66b5df89b3c7f3d1fbe73f555&amp;id=23b4ecb68f" >
+          <div className="form">
             <div className="font">
               <div className="formTitle">Subscribe to our publications</div>
               <input
-                type="email"
-                name="EMAIL"
                 className="aboutInput"
-                id="mce-EMAIL"
+                type={"text"}
+                value={value}
+                name={"subscribe"}
+                onChange={(e)=>{handleChange(e)}}
+                id={"subscribe"}
               />
-              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="aboutButton" ></input>
+              <div className="aboutButton" onClick={()=>{subscribe()}}> Subscribe</div>
             </div>
             <div className="absolute"></div>
-          </form>
+          </div>
         </div>
         <div className="description">Principle Ventures is a native Web3 firm based on data-driven research and BUILD-focused practice, mainly active in crypto innovation investment and digital assets management. We co-build the digital future together with the brightest minds in the industry, support mission-driven builders for excellent engineering and networking, and assist teams strategically in whole growth for a long-term success. We partner with teams to explore possibilities in the full Web3 stack powered by crypto technologies. </div>
          
