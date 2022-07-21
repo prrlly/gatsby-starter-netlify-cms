296b73204a2969e9ec1a893ad6beb80264a39cc1
DDDD <96461773+aurevae@users.noreply.github.com>
2022-07-21 23:08:01 +0800

    test picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     title: "Principle Ventures",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Principle Ventures is a native Web3 firm based on research-driven practice for innovation fabric building, more active in crypto innovation investment and digital assets management.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
